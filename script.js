9cf91a96ab0446d93bb7594c611846b84e0205f0
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-24 16:07:52 +0000

    7-04

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ document.getElementById('button2').addEventListener('click', click2)
 
 function click () {
   age = document.getElementById('age').value
-	if ((dof == 'tuesday' || dof == 'thursday') || (age > 12 && age < 21)) {
-		alert('You get student pricing')
-	} else {
-		alert('you must pay full admission')
-	}
+  if ((dof === 'tuesday' || dof === 'thursday') || (age > 12 && age < 21)) {
+    alert('You get student pricing')
+  } else {
+    alert('you must pay full admission')
+  }
 }
 
 function click2 () {
   dof = document.getElementById('dayofweek').value
-	if ((dof == 'tuesday' || dof == 'thursday') || (age > 12 && age < 21)) {
-		alert('You get student pricing')
-	} else {
-		alert('you must pay full admission')
-	}
+  if ((dof === 'tuesday' || dof === 'thursday') || (age > 12 && age < 21)) {
+    alert('You get student pricing')
+  } else {
+    alert('you must pay full admission')
+  }
 }
